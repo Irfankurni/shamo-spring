44cb19de4b69dce369692dc002c3c9e828343340
IrfanKurni <irfangokil23@gmail.com>
2023-04-23 01:27:51 +0700

    chore:[irfan] upgrade spring boot version to 2.7.11

--- src/main/java/com/example/shamo/security/ApiSecurity.java
+++ src/main/java/com/example/shamo/security/ApiSecurity.java
@@ -6,18 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.example.shamo.service.UserService;
 
 @EnableWebSecurity
-public class ApiSecurity extends WebSecurityConfigurerAdapter {
+public class ApiSecurity {
 
 	@Autowired
 	private UserService userService;
@@ -31,25 +32,25 @@ public class ApiSecurity extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthorizationFilter authFilter;
 
-	@Override
+	@Bean
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().addFilterAfter(authFilter, BasicAuthenticationFilter.class);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		antMatchers.forEach(matcher -> web.ignoring().requestMatchers(matcher));
+		return http.build();
 	}
 
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public WebSecurityCustomizer configure() throws Exception {
+		return (web) -> antMatchers.forEach(matcher -> web.ignoring().requestMatchers(matcher));
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
 }
